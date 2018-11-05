d65a777aa3ba909243e61f5c2e1abd3e46c0b171
andy <andnpatterson@gmail.com>
2018-11-04 18:44:21 -0700

    add printer for finding missing results

--- dist/src/results/findMissing.js
+++ dist/src/results/findMissing.js
@@ -10,19 +10,23 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const utilities_ts_1 = require("utilities-ts");
 const experiments_1 = require("../experiments");
+const printer_1 = require("../utils/printer");
 function findMissing(base_path, path, runs) {
     return __awaiter(this, void 0, void 0, function* () {
         const paths = yield utilities_ts_1.files.glob(`${base_path}/**/test.csv`);
         const raw_exp = yield utilities_ts_1.files.readJson(path, experiments_1.getExperimentSchema());
-        const count = experiments_1.getNumberOfRuns(raw_exp.metaParameters);
+        const count = experiments_1.getNumberOfRuns(raw_exp.metaParameters) * runs;
         const incomplete = [];
-        for (let i = 0; i < count * runs; ++i) {
-            const res_path = experiments_1.ExperimentDescription.getResultsPath(raw_exp, i);
-            const full_path = `${base_path}/${res_path}`;
-            const found = paths.find(p => p.startsWith(full_path));
-            if (!found)
-                incomplete.push(i);
-        }
+        printer_1.printProgress(print => {
+            for (let i = 0; i < count; ++i) {
+                const res_path = experiments_1.ExperimentDescription.getResultsPath(raw_exp, i);
+                const full_path = `${base_path}/${res_path}`;
+                const found = paths.find(p => p.startsWith(full_path));
+                if (!found)
+                    incomplete.push(i);
+                print(`Searching for missing results: ${(i / count).toPrecision(3)}%`);
+            }
+        });
         return incomplete;
     });
 }
--- src/results/findMissing.ts
+++ src/results/findMissing.ts
@@ -1,21 +1,25 @@
 import * as _ from 'lodash';
 import { files } from 'utilities-ts';
 import { getNumberOfRuns, ExperimentDescription, getExperimentSchema } from '../experiments';
+import { printProgress } from '../utils/printer';
 
 export async function findMissing(base_path: string, path: string, runs: number) {
     const paths = await files.glob(`${base_path}/**/test.csv`);
     const raw_exp = await files.readJson(path, getExperimentSchema());
 
-    const count = getNumberOfRuns(raw_exp.metaParameters);
+    const count = getNumberOfRuns(raw_exp.metaParameters) * runs;
     const incomplete = [] as number[];
-    for (let i = 0; i < count * runs; ++i) {
-        const res_path = ExperimentDescription.getResultsPath(raw_exp, i);
+    printProgress(print => {
+        for (let i = 0; i < count; ++i) {
+            const res_path = ExperimentDescription.getResultsPath(raw_exp, i);
 
-        const full_path = `${base_path}/${res_path}`;
+            const full_path = `${base_path}/${res_path}`;
 
-        const found = paths.find(p => p.startsWith(full_path));
-        if (!found) incomplete.push(i);
-    }
+            const found = paths.find(p => p.startsWith(full_path));
+            if (!found) incomplete.push(i);
+            print(`Searching for missing results: ${(i / count).toPrecision(3)}%`);
+        }
+    });
 
     return incomplete;
 }
