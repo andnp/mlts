3c9add96a7ac37fcdfc415df1ead9e0663a8dc89
andy <andnpatterson@gmail.com>
2019-01-09 21:10:51 -0700

    greatly simplify findMissing script

--- dist/src/results/findMissing.js
+++ dist/src/results/findMissing.js
@@ -5,19 +5,20 @@ const experiments_1 = require("../experiments");
 const printer_1 = require("../utils/printer");
 function findMissing(base_path, path, runs) {
     return printer_1.printProgress(print => {
+        let exp;
         return utilities_ts_1.Observable.fromPromises([utilities_ts_1.files.readJson(path, experiments_1.getExperimentSchema())])
-            .map(raw_exp => ({ exp: raw_exp, count: experiments_1.getNumberOfRuns(raw_exp.metaParameters) * runs }))
-            .flatMap(d => utilities_ts_1.arrays.range(d.count).map(i => ({ i, ...d })))
+            .subscribe(raw_exp => exp = raw_exp)
+            .flatMap(() => utilities_ts_1.arrays.range(experiments_1.getNumberOfRuns(exp.metaParameters) * runs))
             // make sure process doesn't run out of memory servicing too many missing files
             .bottleneck(256)
-            .filter(async (data) => {
-            const res_path = experiments_1.ExperimentDescription.getResultsPath(data.exp, data.i);
+            .filter(async (i) => {
+            const res_path = experiments_1.ExperimentDescription.getResultsPath(exp, i);
             const full_path = `${base_path}/${res_path}/test.csv`;
             const found = await utilities_ts_1.files.fileExists(full_path);
-            print(`Searching for missing results: ${((data.i / data.count) * 100).toPrecision(3)}%`);
+            const count = experiments_1.getNumberOfRuns(exp.metaParameters) * runs;
+            print(`Searching for missing results: ${((i / count) * 100).toPrecision(3)}%`);
             return !found;
-        })
-            .map(data => data.i);
+        });
     });
 }
 exports.findMissing = findMissing;
--- src/results/findMissing.ts
+++ src/results/findMissing.ts
@@ -1,25 +1,26 @@
 import * as _ from 'lodash';
 import { files, arrays, Observable } from 'utilities-ts';
-import { getNumberOfRuns, ExperimentDescription, getExperimentSchema } from '../experiments';
+import { getNumberOfRuns, ExperimentDescription, getExperimentSchema, ExperimentJson } from '../experiments';
 import { printProgress } from '../utils/printer';
 
 export function findMissing(base_path: string, path: string, runs: number) {
     return printProgress(print => {
+        let exp: ExperimentJson;
         return Observable.fromPromises([ files.readJson(path, getExperimentSchema()) ])
-            .map(raw_exp => ({ exp: raw_exp, count: getNumberOfRuns(raw_exp.metaParameters) * runs}))
-            .flatMap(d => arrays.range(d.count).map(i => ({ i, ...d })))
+            .subscribe(raw_exp => exp = raw_exp)
+            .flatMap(() => arrays.range(getNumberOfRuns(exp.metaParameters) * runs))
             // make sure process doesn't run out of memory servicing too many missing files
             .bottleneck(256)
-            .filter(async (data) => {
-                const res_path = ExperimentDescription.getResultsPath(data.exp, data.i);
+            .filter(async (i) => {
+                const res_path = ExperimentDescription.getResultsPath(exp, i);
                 const full_path = `${base_path}/${res_path}/test.csv`;
                 const found = await files.fileExists(full_path);
 
-                print(`Searching for missing results: ${((data.i / data.count) * 100).toPrecision(3)}%`);
+                const count = getNumberOfRuns(exp.metaParameters) * runs;
+                print(`Searching for missing results: ${((i / count) * 100).toPrecision(3)}%`);
 
                 return !found;
-            })
-            .map(data => data.i);
+            });
     });
 
 }
