f5d8ce1b4b81a3db79a1cec45e341deb36107f52
andy <andnpatterson@gmail.com>
2019-02-16 18:54:05 -0700

    try to upgrade to latest tf-node, too many bugs

--- dist/tests/integration/algorithms/SupervisedAutoencoder.test.js
+++ dist/tests/integration/algorithms/SupervisedAutoencoder.test.js
@@ -22,7 +22,7 @@ test('Can separate easy difficulty randomly generated dataset with near perfect 
         iterations: 50,
         batchSize: 10,
         type: 'rmsprop',
-        learningRate: 0.001,
+        learningRate: 0.01,
     },
     error: 0.1,
 }));
@@ -44,7 +44,7 @@ test('Can separate medium difficulty randomly generated dataset with high accura
         iterations: 20,
         batchSize: 10,
         type: 'rmsprop',
-        learningRate: 0.001,
+        learningRate: 0.01,
     },
     error: 0.2,
 }));
@@ -66,7 +66,7 @@ test('Can separate hard difficulty randomly generated dataset with high accuracy
         iterations: 20,
         batchSize: 10,
         type: 'rmsprop',
-        learningRate: 0.001,
+        learningRate: 0.01,
     },
     error: 0.4,
 }));
--- dist/tests/utils.d.ts
+++ dist/tests/utils.d.ts
@@ -1,7 +1,7 @@
 import { SupervisedDictionaryLearningDatasetDescription, TensorflowDataset } from 'data';
 import { SupervisedAlgorithm, ReconstructionAlgorithm, UnsupervisedAlgorithm } from "algorithms";
 import { OptimizationParameters } from "optimization";
-export declare function getFakeClassificationDataset(description: SupervisedDictionaryLearningDatasetDescription, difficulty?: 'easy' | 'medium' | 'hard'): TensorflowDataset;
+export declare function getFakeClassificationDataset(description: SupervisedDictionaryLearningDatasetDescription, difficulty?: 'easy' | 'medium' | 'hard'): any;
 interface ClassificationTestParameters {
     alg: SupervisedAlgorithm;
     dataset: TensorflowDataset;
